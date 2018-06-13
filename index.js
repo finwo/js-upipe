f59f75e1ced3dc5f3fca789b0884f0c9008c7295
Robin Bron <robin@finwo.nl>
2018-06-13 17:51:48 +0200

    functional version

--- index.js
+++ index.js
@@ -1,63 +1,70 @@
-var batch = require('./lib/batch'),
-    EE    = require('./lib/ee');
-
-function process(context) {
-  if (!context.queue.length) return;
-  batch(context, context.transform || context.emit.bind(undefined, 'data'), context.queue);
-  context.queue = [];
-}
-
-var proto = {
-  finished    : false,
-  writable    : true,
-  transform   : false,
-  queue       : [],
-
-  end: function(chunk) {
-    if ('undefined' !== chunk) this.write(chunk);
-    if (!this.finished) {
-      this.write(null);
-      this.finished = true;
-      this.emit('end');
-    }
-  },
-  write: function(chunk) {
-    this.queue.push(chunk);
-    process(this);
-  },
-  pipe: function(destination) {
-    switch(typeof destination) {
-      case 'function':
-        return this.pipe(upipe(destination));
-      case 'object':
-        if (!destination.writable) return this;
-        this.on('data', function(chunk) {
-          if ( null === chunk ) return destination.end();
-          destination.write(chunk);
-        });
-        return destination;
-    }
-    return this;
+var upipe = module.exports = (function() {
+  function dq(ctx) {
+    if(!ctx.q.length) return;
+    ctx.q = ctx.q.filter(function(dat) {
+        (ctx.t||ctx.emit.bind(undefined,'data'))(dat);
+    });
   }
-};
-
-var upipe = module.exports = function(data) {
-  var out = EE(Object.create(proto));
-  setTimeout(process.bind(undefined,out),0);
-  out.on('end', function() {
-    if(!this.finished) {
-      this.write(null);
-      this.finished = true;
-    }
-  });
-  if ( 'function' === typeof data ) {
-    out.transform = data;
-  } else if ( 'undefined' !== typeof data ) {
-    if (Array.isArray(data)) {
-      out.queue.push.apply(out.q,data);
-    } else {
-      out.queue.push(data);
-    }
+  function EE(sub) {
+    var l={};sub=sub||this;
+    if((global||window)===sub) return EE({});
+    return Object.assign(sub,{
+      on: (n,h)=>((l[n]=l[n]||[]).push(h),sub),
+      emit: function(n,a) {
+        a=[].slice.call(arguments);n=a.shift();
+        (l[n]||[]).forEach((f)=>f.apply(sub,a));
+      }
+    });
   }
-  return out;
-};
+  var p = {
+    writable : true,
+    t        : false,
+    q        : [],
+    end: function(chunk) {
+      if('undefined'!==typeof chunk) this.write(chunk);
+      if(this.writable) {
+        this.write(null);
+        this.writable = false;
+        this.emit('end');
+      }
+    },
+    write: function(chunk) {
+      this.q.push(chunk);
+      dq(this);
+    },
+    pipe: function( dest ) {
+      switch(typeof dest) {
+        case 'function': return this.pipe(upipe(dest));
+        case 'object':
+          if(!dest.writable) return this;
+          this.on('data', function(chunk) {
+            if ( null === chunk ) return dest.end();
+            if ( dest.writable ) dest.write(chunk);
+          });
+          return dest;
+      }
+      return this;
+    }
+  };
+  return function(dat) {
+    var out = EE(Object.create(p));
+    out.q = [];
+    setTimeout(dq.bind(undefined,out),0);
+    out.on('end',function() {
+      if(this.writable) {
+        this.write(null);
+        this.writable = false;
+      }
+    });
+    if ( 'function' === typeof dat ) {
+      out.t = dat;
+    } else if ( 'undefined' !== typeof dat ) {
+      if (Array.isArray(dat)) {
+        out.q.push.apply(out.q,dat);
+      } else {
+        out.q.push(dat);
+      }
+    }
+    return out;
+  }
+})();
